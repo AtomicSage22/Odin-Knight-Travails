48376378635d120ba769bd171e9f211b82c502e6
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-06-06 23:58:47 +0530

    change functions to support graph

--- script.js
+++ script.js
@@ -1,40 +1,80 @@
-const chessBoard = [];
+const gameBoard = new Map();
+
+const knightMoves = [
+  [2, 1],
+  [1, 2],
+  [-2, 1],
+  [-1, 2],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+];
+
 for (let i = 0; i < 8; i++) {
-	chessBoard[i] = [];
+  for (let j = 0; j < 8; j++) {
+    gameBoard.set([i, j], []);
+  }
 }
 
-const addMove = (x, y, score) =>{
-    if(x >= 0 && y >= 0 && x <= 7 && y <= 7 && chessBoard[x][y] == null){
-        chessBoard[x][y] = score;
+const iterator = gameBoard.keys();
+
+for (let key of iterator) {
+  let i = key[0];
+  let j = key[1];
+
+  const value = gameBoard.get(key);
+
+  knightMoves.forEach((move) => {
+    const x = move[0];
+    const y = move[1];
+
+    const finalValue = [i + x, j + y];
+
+    value.push(finalValue);
+  });
+}
+
+function cleanMoves() {
+  const iterator = gameBoard.keys();
+
+  for (let key of iterator) {
+    let values = gameBoard.get(key);
+
+    const filteredValues = values.filter(
+      (value) =>
+        value[0] >= 0 && value[1] >= 0 && value[0] <= 7 && value[1] <= 7
+    );
+
+    gameBoard.set(key, filteredValues);
+  }
+}
+
+function hasPath(gameBoard, src, dest) {
+  if (src.toString() === dest.toString()) return 0;
+
+  const queue = [[src, 0, []]];
+
+  while (queue.length > 0) {
+    const [current, distance, pathArr] = queue.shift();
+
+    if (current.toString() === dest.toString()) {
+      pathArr.forEach((path) => {
+        console.log(`[${path.toString()}]`);
+      });
+      return distance;
     }
+
+    for (let keypairs of gameBoard) {
+      if (current.toString() === keypairs[0].toString()) {
+        for (let value of gameBoard.get(keypairs[0])) {
+          queue.push([value, distance + 1, [...pathArr, value]]);
+        }
+      }
+    }
+  }
+
+  return false;
 }
 
-const addAllMoves = (x, y, score) => {
-    addMove(x + 2, y + 1, score);
-    addMove(x + 1, y + 2, score);
-    addMove(x - 2, y + 1, score);
-    addMove(x - 2, y - 1, score);
-    addMove(x - 1, y + 2, score);
-    addMove(x - 1, y - 2, score);
-    addMove(x + 2, y - 1, score);
-    addMove(x + 1, y - 2, score);
-}
-
-const addAllPossible = (score) => {
-	for (let i = 0; i < 8; i++) {
-		for (let j = 0; j < 8; j++) {
-			if (chessBoard[i][j] === score) {
-				addAllMoves(i, j, score + 1);
-			}
-		}
-	}
-};
-
-const findShortestPath= (startX, startY, endX, endY) =>{
-    addMove(startX, startY, 0);
-    let count = 0;
-    do{
-        addAllPossible(count++);
-    }while(chessBoard[endX][endY] == null)
-    return chessBoard[endX][endY]
-}
+cleanMoves();
